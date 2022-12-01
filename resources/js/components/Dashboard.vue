6c513933e98ffb95898d72560b71304b1fafde3a
Nathaniel Tan <nt.xw33d@gmail.com>
2022-12-01 13:04:28 +0800

    Remove 123 in Dash board

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -49,7 +49,7 @@
                     <div class="card-header">
                         <h3 class="card-title">
                             <i class="fas fa-chart-pie mr-1"></i>
-                            Sales 123
+                            Sales
                         </h3>
                         <div class="card-tools">
                             <ul class="nav nav-pills ml-auto">
