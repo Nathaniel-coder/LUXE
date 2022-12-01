0dbe0cc07b3a70f4c869d79b7010b415356b04ce
Nathaniel Tan <nt.xw33d@gmail.com>
2022-12-01 13:03:12 +0800

    asdfghjkl;lkjhgfd

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -49,7 +49,7 @@
                     <div class="card-header">
                         <h3 class="card-title">
                             <i class="fas fa-chart-pie mr-1"></i>
-                            Sales
+                            Sales 123
                         </h3>
                         <div class="card-tools">
                             <ul class="nav nav-pills ml-auto">
